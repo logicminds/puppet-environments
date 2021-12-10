669182f9b0ec9c73d1460cea3fef587345352e44
Corey Osman <corey@nwops.io>
2021-12-09 17:30:29 -0800

    Add logging and check for git command

--- r10k-environments-git.sh
+++ r10k-environments-git.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
 export PATH:$PATH:/usr/bin
-
+GIT_URL="https://github.com/nwops/puppet-environments.git"
+GIT_COMMAND=$(which git)
+LOG_FILE='/var/log/puppetserver/r10k-yaml.log'
+if [[ $? -ne 0 ]]; 
+  echo "\nGit does not appear to be installed or in the PATH\n"
+  exit 1
+fi
 # This is a sample yaml environment exec command you can use to dynamically generate a environments yaml content
 # /etc/puppetlabs/r10k/r10k-environments.sh
 if [[ -f /etc/puppetlabs/r10k/puppet-environments/r10k-environments.yaml ]]; then
   cd /etc/puppetlabs/r10k/puppet-environments
-  git pull > /dev/null 2>&1
+  ${GIT_COMMAND} pull 2>&1 > ${LOG_FILE}
 else
-  git clone https://github.com/nwops/puppet-environments.git /etc/puppetlabs/r10k/puppet-environments > /dev/null 2>&1
+  ${GIT_COMMAND} clone -q $GIT_URL /etc/puppetlabs/r10k/puppet-environments 2>&1 > ${LOG_FILE}
 fi
 if [[ $? -ne 0 ]]; then
-  echo "Something happen and the script did not execute correctly"
+  echo "\nSomething happen and the script did not execute correctly\n"
   exit 1
 fi
 cat /etc/puppetlabs/r10k/puppet-environments/r10k-environments.yaml | sed 's|[[:blank:]]*#.*||;/./!d;'
