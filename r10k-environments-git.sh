460b50fbd4cc318463fd4b7e555f1a881f8bbfa7
Corey Osman <corey@nwops.io>
2021-12-09 17:37:18 -0800

    Adds rebase when pulling and fixes condition statement

--- r10k-environments-git.sh
+++ r10k-environments-git.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-export PATH:$PATH:/usr/bin
 GIT_URL="https://github.com/nwops/puppet-environments.git"
 GIT_COMMAND=$(which git)
-LOG_FILE='/var/log/puppetserver/r10k-yaml.log'
-if [[ $? -ne 0 ]]; 
+LOG_FILE='/var/log/puppetlabs/r10k/r10k-yaml.log'
+if [[ $? -ne 0 ]]; then
   echo "\nGit does not appear to be installed or in the PATH\n"
   exit 1
 fi
@@ -12,9 +11,9 @@ fi
 # /etc/puppetlabs/r10k/r10k-environments.sh
 if [[ -f /etc/puppetlabs/r10k/puppet-environments/r10k-environments.yaml ]]; then
   cd /etc/puppetlabs/r10k/puppet-environments
-  ${GIT_COMMAND} pull 2>&1 > ${LOG_FILE}
+  ${GIT_COMMAND} pull --rebase 2>&1 >> ${LOG_FILE}
 else
-  ${GIT_COMMAND} clone -q $GIT_URL /etc/puppetlabs/r10k/puppet-environments 2>&1 > ${LOG_FILE}
+  ${GIT_COMMAND} clone -q $GIT_URL /etc/puppetlabs/r10k/puppet-environments 2>&1 >> ${LOG_FILE}
 fi
 if [[ $? -ne 0 ]]; then
   echo "\nSomething happen and the script did not execute correctly\n"
