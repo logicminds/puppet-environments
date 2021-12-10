5388bee10cf30d381b65d9b9303cc5d675a72f32
Corey Osman <corey@nwops.io>
2021-12-09 18:10:39 -0800

    Add additionally commentary when run as root

--- r10k-environments-git.sh
+++ r10k-environments-git.sh
@@ -2,7 +2,11 @@
 
 GIT_URL="https://github.com/nwops/puppet-environments.git"
 GIT_COMMAND=$(which git)
-LOG_FILE='/tmp/r10k-yaml.log'
+BASE_SERVER_DIR="/opt/puppetlabs/server/data/puppetserver"
+REPO_DIR="${BASE_SERVER_DIR}/puppet-environments"
+LOG_FILE="${BASE_SERVER_DIR}/r10k-yaml.log"
+YAML_FILE="${REPO_DIR}/r10k-environments.yaml"
+
 if [[ $? -ne 0 ]]; then
   echo "\nGit does not appear to be installed or in the PATH\n"
   exit 1
@@ -16,15 +20,22 @@ if [[ $? -ne 0 ]]; then
 fi
 # This is a sample yaml environment exec command you can use to dynamically generate a environments yaml content
 # /etc/puppetlabs/r10k/r10k-environments.sh
-if [[ -f /etc/puppetlabs/r10k/puppet-environments/r10k-environments.yaml ]]; then
-  cd /etc/puppetlabs/r10k/puppet-environments
+if [[ -f ${YAML_FILE} ]]; then
+  cd ${REPO_DIR}
   ${GIT_COMMAND} pull --rebase 2>&1 >> ${LOG_FILE}
 else
-  ${GIT_COMMAND} clone -q $GIT_URL /etc/puppetlabs/r10k/puppet-environments 2>&1 >> ${LOG_FILE}
+  ${GIT_COMMAND} clone -q $GIT_URL ${REPO_DIR} 2>&1 >> ${LOG_FILE}
 fi
 if [[ $? -ne 0 ]]; then
   echo "\nSomething happen and the script did not execute correctly\n"
   echo "Check log file ${LOG_FILE}"
   exit 1
 fi
-cat /etc/puppetlabs/r10k/puppet-environments/r10k-environments.yaml | sed 's|[[:blank:]]*#.*||;/./!d;'
+
+cat ${YAML_FILE} | sed 's|[[:blank:]]*#.*||;/./!d;'
+if [[ $(id -u) -eq 0 ]]; then
+  # ran as root, if we don't delete our tracks then code manager fails
+  echo "Please remove ${LOG_FILE}"
+  echo "Please remove ${REPO_DIR}"
+  echo "Code manager does not work due to permissions"
+fi
